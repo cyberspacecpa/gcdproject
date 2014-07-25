4d186839b482519f48caddb688bf1703b14a4024
cyberspacecpa <cyberspacecpa@users.noreply.github.com>
2014-07-25 03:56:28 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -27,8 +27,8 @@ vlab <- dlab[, 2] # Eliminate the row names after reading the variable labels
 vlab <- gsub("\\(","",vlab) # Remove opened parenthesis
 vlab <- gsub("\\)","",vlab) # Remove closed parenthesis
 vlab <- gsub("\\-","",vlab) # Remove dash
-vlab <- gsub("^t","avgTime",vlab) # set prefix 1 Time
-vlab <- gsub("^f","avgFreq",vlab) # set prefix 2 Frequency
+vlab <- gsub("^t","avgTime",vlab) # set prefix 1 avgTime
+vlab <- gsub("^f","avgFreq",vlab) # set prefix 2 avgFrequency
 vlab <- gsub("meanF","MeanF",vlab)# Differentiate MeanFreq from mean
 vlab <- gsub("BodyBody","Body",vlab) # Remove duplication
 
